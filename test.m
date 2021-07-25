2dc9507b8ac3958f5d2c9dace1a7ae759ae5331f
LYY <LiYanyi19981104@163.com>
2021-07-25 12:49:43 +0800

    Update test.m

--- test.m
+++ test.m
@@ -2,8 +2,15 @@ clear all
 close all
 clc
 
-results_path = './log/g8/';
-testset_path  = './itzik/MatlabProjects/3DmFVNet/data/ModelNet40/test/';
+%old path
+% results_path = './log/g8/';
+% testset_path  = './itzik/MatlabProjects/3DmFVNet/data/ModelNet40/test/';
+
+%new path in my computer
+results_path = 'E:/LYYgithub/3DmFV_Data/log/g8_1024';
+testset_path  = 'E:/LYYgithub/3DmFV_Data/data/ModelNet40/test/';
+
+
 load([results_path, '3DmFV_Net.mat']);
 
 [test_pc_ds] = pc_3dmfv_data_store(testset_path, n_points, GMM, normalize, flatten, false, augmentations);
